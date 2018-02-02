0a12b3e3ff5a64d275d724e83bb47d4d0b4e13e0
hirata <ddjt87@gmail.com>
2018-02-02 14:13:01 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -6,16 +6,24 @@
     
     <h1>タスク新規作成ページ</h1>
     
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+     <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                    {!! Form::submit('作成', ['class' => 'btn btn-primary']) !!}
+        
+            {!! Form::close() !!}
+        </div>
+    </div>
     
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-        
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-        
-        {!! Form::submit('作成') !!}
-        
-    {!! Form::close() !!}
-
 @endsection
